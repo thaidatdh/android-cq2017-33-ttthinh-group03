f02f5f653368ac0aec59a4705332959daf21f9d6
hoangtiendat <dat13.5.1999@gmail.com>
2019-10-12 09:48:56 +0700

    edit insert User

--- app/src/main/java/com/hcmus/DAO/UserDao.java
+++ app/src/main/java/com/hcmus/DAO/UserDao.java
@@ -3,10 +3,7 @@ package com.hcmus.DAO;
 import com.hcmus.DTO.UserDto;
 import com.hcmus.Utils.Database;
 
-import java.sql.Connection;
 import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,27 +11,28 @@ public class UserDao {
     public UserDao() {
     }
 
-    public static List<UserDto> SelectAll() throws SQLException {
+    public static List<UserDto> SelectAll() {
         List<UserDto> user = new ArrayList<>();
         String sql = "select * from Users";
-        ResultSet rs = Database.SelectQuery(sql);
-        while (rs.next()){
-            user.add(new UserDto(rs.getInt("user_id"), rs.getString("user_type").charAt(0), rs.getString("username"), rs.getString("password"), rs.getString("firstname"), rs.getString("lastname"), rs.getString("birth_date"), rs.getString("address"), rs.getString("phone"), rs.getString("created_date")));
-        }
+        try {
+            ResultSet rs = Database.SelectQuery(sql);
+            while (rs.next()){
+                user.add(new UserDto(rs.getInt("user_id"), rs.getString("user_type").charAt(0), rs.getString("username"), rs.getString("password"), rs.getString("firstname"), rs.getString("lastname"), rs.getString("birth_date"), rs.getString("address"), rs.getString("phone"), rs.getString("created_date")));
+            }
+        } catch (Exception ex) {}
         return user;
     }
 
-    public static boolean Insert(UserDto user) throws SQLException {
+    public static int Insert(UserDto user) {
         String sql = "INSERT INTO Users(username, password, firstname, lastname, birth_date, user_type, address, phone, created_date) VALUES('"
                 + user.getUsername() + "', '" + user.getPassword() + "', '" + user.getFirstName() + "', '" + user.getLastName() + "', '" + user.getBirthDate() + "', '" + user.getUserType() + "', '" + user.getAddress() + "', '" + user.getPhone() + "', '" + user.getCreatedDate() + "')";
-        if (Database.ExecuteQuery(sql) > 0) {
-            return true;
-        } else {
-            return false;
-        }
+        int result = Database.ExecuteQuery(sql);
+        if (result == -1)
+            return result;
+        return Database.GetLastestId("users","user_id");
     }
 
-    public static boolean Update(UserDto user) throws SQLException {
+    public static boolean Update(UserDto user) {
         String sql = "UPDATE Users set username = '" + user.getUsername() + "',  password = '" + user.getPassword() + "',  firstname = '"
                 + user.getFirstName() + "',  lastname = '" + user.getLastName() + "',  birth_date = '" + user.getBirthDate() + "',  user_type = '"
                 + user.getUserType() + "',  address = '" + user.getAddress() + "',  phone = '" + user.getPhone() + "',  created_date = '" + user.getCreatedDate() + "' WHERE user_id = " + user.getUserId();
@@ -44,7 +42,7 @@ public class UserDao {
         return false;
     }
 
-    public static boolean Delete(UserDto user) throws SQLException {
+    public static boolean Delete(UserDto user) {
         String sql = "delete from Users where user_id = " + user.getUserId();
         if (Database.ExecuteQuery(sql) > 0) {
             return true;
@@ -53,13 +51,15 @@ public class UserDao {
     }
 
 
-    public static UserDto findById(int Id) throws SQLException {
+    public static UserDto findById(int Id) {
         UserDto user = new UserDto();
         String sql = "Select * from Users WHERE use_id = " + Id;
-        ResultSet rs = Database.SelectQuery(sql);
-        while (rs.next()){
-            user = new UserDto(rs.getInt("user_id"), rs.getString("user_type").charAt(0), rs.getString("username"), rs.getString("password"), rs.getString("firstname"), rs.getString("lastname"), rs.getString("birth_date"), rs.getString("address"), rs.getString("phone"), rs.getString("created_date"));
-        }
+        try {
+            ResultSet rs = Database.SelectQuery(sql);
+            while (rs.next()){
+                user = new UserDto(rs.getInt("user_id"), rs.getString("user_type").charAt(0), rs.getString("username"), rs.getString("password"), rs.getString("firstname"), rs.getString("lastname"), rs.getString("birth_date"), rs.getString("address"), rs.getString("phone"), rs.getString("created_date"));
+            }
+        } catch (Exception ex) {}
         return user;
     }
 }
