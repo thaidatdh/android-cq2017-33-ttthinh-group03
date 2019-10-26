e06932545836de076822294e22b08ee9fcdc23bd
cuongvd10 <vongdatcuong280599@gmail.com>
2019-10-26 15:09:03 +0700

    [Add - Shipper Task]: click task item to show alert dialog along with bill information

--- app/src/main/java/com/hcmus/DAO/BillDao.java
+++ app/src/main/java/com/hcmus/DAO/BillDao.java
@@ -113,12 +113,12 @@ public class BillDao {
         } catch (Exception ex) {}
         return bill;
     }
-    public static List<Task> GetTaskOfShipper(int shipper){
+    public static List<Task> GetAllAvailableTask(){
         List<Task> bill = new ArrayList<>();
         String sql = "select * " +
                 "from users us inner join bill " +
                 "on us.user_id = bill.customer " +
-                "where bill.shipper = " + shipper;
+                "where us.type = 'CUSTOMER' AND bill.shipper is NULL";
         try {
             ResultSet rs = Database.SelectQuery(sql);
             while (rs.next()){
@@ -126,6 +126,28 @@ public class BillDao {
             }
         } catch (Exception ex) {}
         return bill;
+    }
+    public static List<Task> GetTaskOfShipper(int shipper){
+        List<Task> bill = new ArrayList<>();
+        String sql = "select * " +
+                "from users us inner join bill " +
+                "on us.user_id = bill.customer " +
+                "where us.type = 'CUSTOMER' AND bill.shipper = " + shipper;
+        try {
+            ResultSet rs = Database.SelectQuery(sql);
+            while (rs.next()){
+                bill.add(new Task(rs.getInt("bill_id"), rs.getInt("customer"), rs.getString("created_date"), rs.getString("description"), rs.getLong("total_price"), rs.getLong("ship_charge"), rs.getBoolean("accepted"), rs.getString("status").charAt(0), rs.getInt("shipper"), rs.getString("deliver_time"), rs.getBoolean("is_completed"), rs.getString("type"), rs.getString("username"), rs.getString("firstname"), rs.getString("lastname"), rs.getString("birth_date"), rs.getString("address"), rs.getString("phone")));
+            }
+        } catch (Exception ex) {}
+        return bill;
+    }
+    public static void AssignShipper(int billId, int shipperID){
+        String sql = "UPDATE bill " +
+                "SET shipper = " + shipperID + " " +
+                "WHERE " + shipperID + " IN (SELECT user_id FROM users ) AND bill_id = " + billId + " AND shipper is NULL";
+        try {
+            ResultSet rs = Database.SelectQuery(sql);
+        } catch (Exception ex) {}
     }
     public static List<BillDto> FindByCustomer(int customer_id){
         List<BillDto> bill = new ArrayList<>();
