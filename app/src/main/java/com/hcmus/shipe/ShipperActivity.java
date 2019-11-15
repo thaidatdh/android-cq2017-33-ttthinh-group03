2aea7676a7bfd524402690da24ce50848a3f4a8e
htdat <dat13.5.1999@gmail.com>
2019-11-15 22:02:35 +0700

    permission Location + GPS

--- app/src/main/java/com/hcmus/shipe/ShipperActivity.java
+++ app/src/main/java/com/hcmus/shipe/ShipperActivity.java
@@ -1,7 +1,10 @@
 package com.hcmus.shipe;
 
 import android.Manifest;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
@@ -14,11 +17,13 @@ import com.google.android.material.tabs.TabItem;
 import com.google.android.material.tabs.TabLayout;
 
 import androidx.appcompat.app.ActionBar;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.hcmus.Activities.ManagerActivity;
 import com.hcmus.DAO.BillDao;
 import com.hcmus.DAO.UserDao;
 import com.hcmus.DTO.UserDto;
@@ -52,6 +57,8 @@ public class ShipperActivity extends AppCompatActivity implements LocationListen
     private ShipperTaskFragment shipperTask;
     private ShipperOrderFragment shipperOrder;
     private ShipperMapFragment shipperMap;
+    public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
+    String provider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -143,6 +150,10 @@ public class ShipperActivity extends AppCompatActivity implements LocationListen
 
             }
         });
+        checkLocationPermission();
+        if (!mLocationManager.isProviderEnabled (LocationManager.GPS_PROVIDER)) {
+            buildAlertMessageNoGps();
+        }
     }
     @Override
     public void onLocationChanged(Location location) {
@@ -167,5 +178,67 @@ public class ShipperActivity extends AppCompatActivity implements LocationListen
     public void onBackPressed() {
         //Login.Logout(getApplicationContext());
         //finishAndRemoveTask();
+    }
+
+    public boolean checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_LOCATION);
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // location-related task you need to do.
+                    if (ContextCompat.checkSelfPermission(this,
+                            Manifest.permission.ACCESS_FINE_LOCATION)
+                            == PackageManager.PERMISSION_GRANTED) {
+
+                        //Request location updates:
+                        mLocationManager.requestLocationUpdates(provider, 400, 1, this);
+                    }
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+
+                }
+                return;
+            }
+
+        }
+    }
+
+    private void buildAlertMessageNoGps() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Your GPS seems to be disabled, do you want to enable it?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(final DialogInterface dialog, final int id) {
+                        startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(final DialogInterface dialog, final int id) {
+                        dialog.cancel();
+                    }
+                });
+        final AlertDialog alert = builder.create();
+        alert.show();
     }
 }
